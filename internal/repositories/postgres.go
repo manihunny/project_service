c79384573003e5a03fd05d027ca36691affe0fb8
Tariel Musaev <uzumake02@mail.ru>
2024-11-11 15:48:31 +0400

    Edit config and gitignore, add database connection logging

--- internal/repositories/postgres.go
+++ internal/repositories/postgres.go
@@ -16,8 +16,6 @@ func InitPostgres(config *config.Config) (*gorm.DB, error) {
 	dbPassword := config.DBPassword
 	dbName := config.DBName
 
-	log.Printf("DB_HOST: %s, DB_PORT: %s, DB_USER: %s, DB_NAME: %s", dbHost, dbPort, dbUser, dbName)
-
 	// Формирование строки подключения
 	dbURI := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
 		dbHost, dbPort, dbUser, dbName, dbPassword)
--- internal/repositories/redis.go
+++ internal/repositories/redis.go
@@ -1,6 +1,9 @@
 package repositories
 
 import (
+	"fmt"
+	"log"
+
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/redis/go-redis/v9"
 	"gitlab.fast-go.ru/fast-go-team/project/config"
@@ -12,6 +15,12 @@ func InitRedis(config *config.Config) (*redis.Client, error) {
 	dbUser := config.RedisDBUser
 	dbPassword := config.RedisDBPassword
 
+	// Формирование строки подключения
+	dbURI := fmt.Sprintf("host=%s port=%s user=%s password=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword)
+
+	log.Printf("Connecting to Redis with URI: %s", dbURI)
+	
 	// Подключение к базе данных
 	db := redis.NewClient(&redis.Options{
 		Addr:     dbHost + dbPort,
